a39de12cca1b4720095bfeeadf43111ccabfc9a0
kohama66 <54892265+kohama66@users.noreply.github.com>
2024-05-16 00:05:50 +0900

    feat: list view

--- app.vue
+++ app.vue
@@ -2,22 +2,55 @@
 import {
   getDownloadURL,
   getStorage,
+  listAll,
   ref as storageRef,
+  uploadBytes,
 } from "firebase/storage";
 
 const urls = ref<string[]>([]);
 
 onMounted(async () => {
   const storage = getStorage();
-  const url = await getDownloadURL(storageRef(storage, "neko.jpeg"));
+  const listRef = storageRef(storage);
+  const list = await listAll(listRef);
 
-  urls.value.push(url);
+  list.items.forEach(async (item) => {
+    const url = await getDownloadURL(item);
+    urls.value.push(url);
+  });
 });
+
+async function fileSelect(e: Event) {
+  const target = e.target as HTMLInputElement;
+  const file = target.files?.item(0);
+
+  if (!file) {
+    return;
+  }
+
+  const storage = getStorage();
+  const name = Math.random();
+
+  const _storageRef = storageRef(storage, name.toString());
+  const result = await uploadBytes(_storageRef, file);
+  console.log(result);
+}
 </script>
 
 <template>
-  <div>
-    <h1>Cloud Storage for Firebase</h1>
-    <img v-for="url in urls" :key="url" :src="url" alt="image" />
+  <div style="border: 1px black solid; padding: 8px; margin-bottom: 20px">
+    <h1>画像一覧</h1>
+
+    <div v-for="url in urls" :key="url">
+      <img :src="url" alt="image" />
+    </div>
+  </div>
+
+  <div style="border: 1px black solid; padding: 8px">
+    <h1>upload</h1>
+
+    <form>
+      <input type="file" @change="fileSelect" />
+    </form>
   </div>
 </template>
